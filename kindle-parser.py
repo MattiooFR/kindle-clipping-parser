5f47274db06cb9224403d98a52e90adb18bf72f2
MattiooFR <dugue.mathieu@gmail.com>
2020-09-06 23:46:37 +0200

    creating Book and Books classes

--- kindle-parser.py
+++ kindle-parser.py
@@ -1,19 +1,52 @@
 import re
 
-strings = []
+
+class Books:
+
+    def __init__(self, title='My Kindle Library', books={}):
+        self.books = books
+
+    def addBook(self, book):
+        if not self.books.get(book.title):
+            self.books[book.title] = book
+
+    def getBook(self, bookTitle):
+        return self.books.get(bookTitle)
+
+    def __str__(self):
+        return "Books in library : " + ", ".join([key for key in self.books.keys()])[:-2]
+
+
+class Book:
+
+    def __init__(self, title='Unknown Title', clippings=[]):
+        self.title = title
+        self.clippings = clippings
+
+    def getClippings(self):
+        return self.clippings
+
+    def __str__(self):
+        return str(self.getClippings())
+
+
+Library = Books()
 
 with open('My Clippings.txt', 'r', encoding='utf8') as reader:
     strings = re.split(r'==========', reader.read())
 
-print(strings)
-books = {}
 for line in strings:
     book_title = line.strip().split("\n")[0]
-    print(book_title)
-    book_clippings = "\n".join(line.split("\n")[3:])
-    if not books.get(book_title):
-        books[book_title] = [book_clippings]
-    else:
-        books[book_title].append(book_clippings)
+    Library.addBook(Book(book_title))
 
-print(books.keys())
+    book_clippings = "\n".join(line.split("\n")[3:])
+    print(book_clippings)
+
+    if not Library.getBook(book_title):
+        Library.books[book_title].clippings = [book_clippings]
+    else:
+        Library.books[book_title].clippings.append(book_clippings)
+
+
+print(Library)
+#print(Library.books['The Elon Musk Blog Series: Wait But Why (Urban, Tim)'])
