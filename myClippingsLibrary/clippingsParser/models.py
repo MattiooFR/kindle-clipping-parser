1d0514b169fe872a3c4dc39b0b86780692ff72fa
MattiooFR <dugue.mathieu@gmail.com>
2020-09-07 19:05:18 +0200

    created Library, Book and Clip models and migrated them

--- myClippingsLibrary/clippingsParser/models.py
+++ myClippingsLibrary/clippingsParser/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Library(models.Model):
+    title = models.CharField(default='My Library', max_length=50)
+
+
+class Book(models.Model):
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    author = models.CharField(default='Unknown Author', max_length=70)
+    read_date = models.DateTimeField('book read date')
+
+
+class Clip(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    content = models.CharField(max_length=5000)
+    location = models.IntegerField(default=0)
