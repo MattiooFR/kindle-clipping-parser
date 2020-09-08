104ade0f0e32d672a5ddb498744eb94d78698560
MattiooFR <dugue.mathieu@gmail.com>
2020-09-08 23:07:16 +0200

    added the str representation method of the models

--- myClippingsLibrary/clippingsParser/models.py
+++ myClippingsLibrary/clippingsParser/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Library(models.Model):
     title = models.CharField(default='My Library', max_length=50)
 
+    def __str__(self):
+        return f"{self.title}"
+
 
 class Book(models.Model):
     library = models.ForeignKey(Library, on_delete=models.CASCADE)
@@ -11,8 +14,14 @@ class Book(models.Model):
     author = models.CharField(default='Unknown Author', max_length=70)
     read_date = models.DateTimeField('book read date')
 
+    def __str__(self):
+        return f"{self.title}, {self.author}, {self.library.title}"
+
 
 class Clip(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     content = models.CharField(max_length=5000)
-    location = models.IntegerField(default=0)
+    book_location = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.book.title}, {self.content}, {self.book_location}"
