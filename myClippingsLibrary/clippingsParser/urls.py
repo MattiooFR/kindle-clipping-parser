509eb92d3b6cdd37cc195c41d91a252ed4dc3bcf
MattiooFR <dugue.mathieu@gmail.com>
2020-09-10 11:28:36 +0200

    added library and book views + urls

--- myClippingsLibrary/clippingsParser/urls.py
+++ myClippingsLibrary/clippingsParser/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:library>/', views.library, name='library'),
+    path('<str:library_title>/<int:book_id>/', views.book, name='book')
 ]
--- myClippingsLibrary/clippingsParser/views.py
+++ myClippingsLibrary/clippingsParser/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from .models import Book
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the Clippings index.")
+
+
+def library(request, library):
+    books = ', '.join([str(b)
+                       for b in Book.objects.filter(library__title=library)])
+    return HttpResponse("This is the library id : {} with these books {}".format(library, books))
+
+
+def book(request, library_title, book_id):
+    return HttpResponse("This is the book page for the book id : {}".format(book_id))
