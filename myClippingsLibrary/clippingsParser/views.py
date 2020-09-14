53309c7e27dc6796d2b6b659574d2f455ff0eb74
MattiooFR <dugue.mathieu@gmail.com>
2020-09-14 17:23:15 +0200

    replaced with django shortcut render

--- myClippingsLibrary/clippingsParser/views.py
+++ myClippingsLibrary/clippingsParser/views.py
@@ -1,38 +1,33 @@
-from django.http import HttpResponse
-from django.template import loader
-
+from django.shortcuts import render
 from .models import Book, Library
 
 
 def index(request):
     librarys = Library.objects.all()
 
-    template = loader.get_template('clippingsParser/index.html')
     context = {
         'librarys': librarys
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'clippingsParser/index.html', context)
 
 
 def library(request, library):
     books = Book.objects.filter(library__title=library)
 
-    template = loader.get_template('clippingsParser/library.html')
     context = {
         'books': books,
         'library': library
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'clippingsParser/library.html', context)
 
 
 def book(request, book_id):
     book = Book.objects.get(pk=book_id)
 
-    template = loader.get_template('clippingsParser/book.html')
     context = {
         'title': book.title,
         'author': book.author,
         'library_title': book.library.title,
         'read_date': book.read_date
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'clippingsParser/book.html', context)
