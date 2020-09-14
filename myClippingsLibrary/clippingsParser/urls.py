66a011ff6bf19b82ae7731ca18820132b137e975
MattiooFR <dugue.mathieu@gmail.com>
2020-09-14 17:39:06 +0200

    adding app_name attribute to app urls.py file

--- myClippingsLibrary/clippingsParser/urls.py
+++ myClippingsLibrary/clippingsParser/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'clippingsParser'
 urlpatterns = [
     path('', views.index, name='index'),
     path('library/<str:library>/', views.library, name='library'),
