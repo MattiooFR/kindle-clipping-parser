742fb054fd115ec4dbe5972b0e860a472280c968
MattiooFR <dugue.mathieu@gmail.com>
2020-10-03 15:24:25 +0200

    refactor urls

--- backend/clippingsParser/urls.py
+++ backend/clippingsParser/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from . import views
 
-app_name = 'clippingsParser'
+app_name = "clippingsParser"
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('library/<str:library>/', views.LibraryView.as_view(), name='library'),
-    path('book/<int:pk>/', views.BookView.as_view(), name='book')
+    path("", views.IndexView.as_view(), name="index"),
+    path("library/<str:library>/", views.LibraryView.as_view(), name="library"),
+    path("book/<int:pk>/", views.BookView.as_view(), name="book"),
 ]
