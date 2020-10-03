4949b58cf3549e0e4c2783fa92911cab54fa4eaf
MattiooFR <dugue.mathieu@gmail.com>
2020-10-03 15:24:04 +0200

    refactor models

--- backend/clippingsParser/models.py
+++ backend/clippingsParser/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 class Library(models.Model):
-    title = models.CharField(default='My Library', max_length=50)
+    title = models.CharField(default="My Library", max_length=50)
 
     def __str__(self):
         return f"{self.title}"
@@ -11,19 +11,18 @@ class Library(models.Model):
 
 class Book(models.Model):
     library = models.ForeignKey(
-        Library, on_delete=models.CASCADE, related_name='libraries')
+        Library, on_delete=models.CASCADE, related_name="libraries"
+    )
     title = models.CharField(max_length=100)
-    author = models.CharField(default='Unknown Author', max_length=70)
-    read_date = models.DateTimeField(
-        'book read date', default=timezone.now)
+    author = models.CharField(default="Unknown Author", max_length=70)
+    read_date = models.DateTimeField("book read date", default=timezone.now)
 
     def __str__(self):
         return f"{self.title}, {self.author}, {self.library.title}"
 
 
 class Clip(models.Model):
-    book = models.ForeignKey(
-        Book, on_delete=models.CASCADE, related_name='clippings')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="clippings")
     content = models.CharField(max_length=5000)
     book_location = models.IntegerField(default=0)
 
